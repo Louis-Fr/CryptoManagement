50eabd29f957aeb7864a57bcc4d8487fb175b1db
Louis Rey <louisreypro@yahoo.com>
2023-07-01 12:32:05 +0200

    Initial Commit

---
+++ CryptoManagement/CryptoManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoManagementApp.swift
+//  CryptoManagement
+//
+//  Created by Louis REY on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
